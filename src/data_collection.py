9aa7d197bca31a3e2dcff024e04f5a485ec40837
fenggao <fgao60@gatech.edu>
2024-04-25 20:52:56 -0400

    Update data_collection.py

--- src/data_collection.py
+++ src/data_collection.py
@@ -6,23 +6,27 @@ import pandas as pd
 symbols = ['AAPL', 'AMZN', 'TSLA', 'JNJ', 'JPM', 'SPY', 'EEM', 'GLD']
 
 # Start and end dates
-start_date = '2023-04-01'
-end_date = '2024-04-01'
+start_date1 = '2023-01-01'
+end_date1 = '2023-12-31'
+start_date2 = '2024-01-01'
+end_date2 = '2024-04-01'
 
 
 # Fetching historical data
-def fetch_data(symbols, start_date, end_date):
+def fetch_data(symbols, start_date1, end_date1, start_date2, end_date2):
     data = {}
     directory = '../data/raw'
     if not os.path.exists(directory):  # Check if directory exists
         os.makedirs(directory)
     for symbol in symbols:
         ticker = yf.Ticker(symbol)
-        data = ticker.history(start=start_date, end=end_date)
-        file_path = os.path.join(directory, f'{symbol}_one_year_data.csv')
-        data.to_csv(file_path)  # Save data to CSV
-        print(f'Data for {symbol} saved to {symbol}_one_year_data.csv')
+        training = ticker.history(start=start_date1, end=end_date1)
+        validation = ticker.history(start=start_date2, end=end_date2)
+        training_path = os.path.join(directory, f'{symbol}_training_data.csv')
+        validation_path = os.path.join(directory, f'{symbol}_validation_data.csv')
+        training .to_csv(training_path)  # Save data to CSV
+        validation.to_csv(validation_path)
+        print(f'Data for {symbol} saved to {symbol}_data.csv')
 
-
-fetch_data(symbols, start_date, end_date)
+fetch_data(symbols, start_date1, end_date1, start_date2, end_date2)
 #%%
